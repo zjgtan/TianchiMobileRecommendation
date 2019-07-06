21a501b539d5919f95019c1b859f3aa434d82914
姚默 <yaomo@bilibili.com>
2019-07-06 19:54:21 +0800

    creative table

--- upload_to_hdfs/creative_table.sh
+++ upload_to_hdfs/creative_table.sh
@@ -14,6 +14,15 @@ ROW FORMAT delimited fields terminated by ','
 LOCATION 'viewfs://jssz-bigdata-cluster/department/buss_product/chenjiawei/mobile_recommendation/tianchi_fresh_comp_train_user/'
 "
 
+start_date=20141118
+while [ $start_date -ne 20141219 ]
+do
+    hive -e "
+    ALERT TABLE sycpb.tmp_mobile_recommendation_user_behavoir ADD PARTITION (log_date = ${start_date}) LOCATION viewfs://jssz-bigdata-cluster/department/buss_product/chenjiawei/mobile_recommendation/tianchi_fresh_comp_train_user/log_date=${start_date}
+    "
+    start_date=`date -d "1 days ${start_date}" +%Y%m%d`
+done
+
 hive -e "
 CREATE EXTERNAL TABLE sycpb.tmp_mobile_recommendation_item(
     item_id string,
