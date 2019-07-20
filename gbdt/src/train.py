79f8c36e563ec6cff2fa6128d6f6fe84453eaef0
zjgtan <zjgtan@126.com>
2019-07-20 20:19:10 +0800

    train

---
+++ gbdt/src/train.py
@@ -0,0 +1,16 @@
+# coding: utf8
+"""Usage:
+    模型训练
+"""
+
+import xgboost as xgb
+
+# 训练集
+file_train = sys.argv[1]
+
+dtrain = xgb.DMatrix(file_train)
+
+param = {'max_depth':2, 'eta':1, 'silent':1, 'objective':'binary:logistic'}
+num_round = 2
+
+bst = xgb.train(param, dtrain, num_round)
