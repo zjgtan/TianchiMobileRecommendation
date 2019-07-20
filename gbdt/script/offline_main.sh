d750489ddc13328c15f0af2b21245392063773c4
zjgtan <zjgtan@126.com>
2019-07-20 19:03:34 +0800

    offline main

--- gbdt/script/offline_main.sh
+++ gbdt/script/offline_main.sh
@@ -1,6 +1,10 @@
 # usage: offline训练和预测
 
+echo "offline train feature extract"
 cat ./data/offline_train.dat | python ./src/featrue_extract.py > data/offline_train.ins
+echo "offline train gensign"
 cat ./data/offline_train.ins | python ./src/gensign.py > data/offline_train.sign
+echo "offline test feature extract"
 cat ./data/offline_test.dat | python ./src/featrue_extract.py > data/offline_test.ins
+echo "offline test gensign"
 cat ./data/offline_test.ins | python ./src/gensign.py > data/offline_test.sign
